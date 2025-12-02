40973c5a101b12f36c1388054d0f0f93e72a17ad
Oleksii <kespa71for@gmail.com>
2025-12-02 11:09:11 +0100

    refactor: app module and port from main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -22,7 +20,5 @@ import { User } from './users/entities/user.entity';
     UsersModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
